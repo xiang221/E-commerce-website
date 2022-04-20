6015222b3bf375888bfaf5e0d74fdd38345b9dac
Xiang <41126704+xiang221@users.noreply.github.com>
2022-04-20 21:21:42 +0800

    Update database.js

--- database.js
+++ database.js
@@ -4,9 +4,8 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
     host: process.env["DB_HOST"],
-    user: process.env["DB_PORT"],
-    password: process.env["DB_USER"],
-    database: process.env["DB_PASS"]
+    user: process.env["DB_USER"],
+    password: process.env["DB_PASS"]
 });
 
 
